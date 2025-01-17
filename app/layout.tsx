6359b2cfa12c47af8ff62d17c649301adaa5d07c
Guilherme Boia <guilherme.dantas.boia.albuquerque@ccc.ufcg.edu.br>
2025-01-16 22:51:07 -0300

    chore: add og-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,24 @@ export default function RootLayout({
 
   return (
     <html lang="en" className="bg-[#08070b] text-gray-400 h-full">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://guilhermeboia.com/" />
+        <meta
+          property="og:title"
+          content="Guilherme Boia | Software Engineer"
+        />
+        <meta
+          property="og:description"
+          content="Undergrad Computer Science student just sharing my thoughts and document my journey in tech."
+        />
+        <meta
+          property="og:image"
+          content="https://guilhermeboia.com/og-image.png"
+        />
+      </head>
       <body className={baseClasses}>
         <ScrollProvider>{children}</ScrollProvider>
       </body>
